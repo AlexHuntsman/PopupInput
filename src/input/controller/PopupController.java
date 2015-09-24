5cf53085bfeac471f8df58c520977d95fe221ae2
Alexander Huntsman <Huntsman.alex101@gmail.com>
2015-09-24 09:59:56 -0600

    Add Usefulness to the progect.

--- src/input/controller/PopupController.java
+++ src/input/controller/PopupController.java
@@ -1,10 +1,75 @@
 package input.controller;
 
+import input.view.PopupDisplay;
+import input.model.Thingy;
+
 public class PopupController
 	{
+		private PopupDisplay myPopups;
+		private Thingy myTestThing;
+		public PopupController()
+		{
+			myPopups = new PopupDisplay();
+		}
+		
 		public void start()
 		{
+			String myName = myPopups.grabAnswer("type in your name");
+			myPopups.showResponce("You typed in: " + myName);
 			
+			String temp = myPopups.grabAnswer("type in your age");
+			int myAge;
+			
+			if(isInteger(temp))
+			{
+				myAge = Integer.parseInt(temp);
+			}
+			else
+			{
+				myAge = -9999999;
+			}
+			myPopups.showResponce("You typed " + myAge);
+			
+			String tempWeight = myPopups.grabAnswer("Type in your weight");
+			double myWeight = Double.parseDouble(tempWeight);
+			//Check at home if it is a double
+			myPopups.showResponce("You typed "+ myWeight);
+			
+			myTestThing = new Thingy(myName, myAge, myWeight);
+			
+		}
+		
+		private boolean isInteger(String input)
+		{
+			Boolean isInt = false;
+			
+			try
+			{
+				int  temp = Integer.parseInt(input);
+				isInt = true;
+			}
+			catch(NumberFormatExeption error)
+			{
+				myPopups.showResponce("not an int - bad value will be used");
+			}
+			return isInt;
+		}
+		
+		private boolean isDouble(String input)
+		{
+			boolean isDouble = false;
+			
+			try
+			{
+				double temp = Double.parseDouble(input);
+				isDouble = true;
+			}
+			catch(NumberFormatExeption error)
+			{
+				myPopups.showResponce("not a double - bad value will be used");
+			}
+			
+			return isDouble;
 		}
 
 	}
---
+++ src/input/model/Thingy.java
@@ -0,0 +1,49 @@
+package input.model;
+
+public class Thingy
+	{
+		private String name;
+		private int age;
+		private double weight;
+		
+		public Thingy()
+		{
+			name = "";
+			age = -99;
+			weight = -.008;
+			
+		}
+		
+		public Thingy(String name, int age, double weight)
+		{
+			this.name = name;
+			this.age = age;
+			this.weight = weight;
+		}
+		public String getName()
+		{
+			return name;
+		}
+		public int getAge()
+		{
+			return age;
+		}
+		public double getWeight()
+		{
+			return weight;
+		}
+		
+		public void setName(String name)
+		{
+			this.name = name;
+		}
+		public void setAge(int age)
+		{
+			this.age = age;
+		}
+		public void setWeight(double weight)
+		{
+			this.weight = weight;
+		}
+
+	}
